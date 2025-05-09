b3f12827ac53749dd214cbefd2ec9807784cdffd
Kristen <harunakristen@gmail.com>
2025-05-09 06:36:30 +0000

    first

---
+++ script.js
@@ -0,0 +1,37 @@
+function convertCurrency() {
+  let amount = parseFloat(document.getElementById("amount").value);
+  let midBody = document.getElementById("result");
+  let dropdown = document.getElementById("allCurrency");
+  let selectedCurrency = dropdown.value;
+
+  
+  fetch("https://open.er-api.com/v6/latest/USD")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let rate = data.rates[selectedCurrency]; 
+      let converted = amount * rate;
+
+      midBody.innerText =
+        amount + " USD = " + converted.toFixed(2) + " " + selectedCurrency;
+    });
+
+ 
+  if (dropdown.options.length === 0) {
+    fetch("https://open.er-api.com/v6/latest/USD")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        let currencies = Object.keys(data.rates);
+
+        currencies.forEach(function (currency) {
+          let option = document.createElement("option");
+          option.value = currency;
+          option.textContent = currency;
+          dropdown.appendChild(option);
+        });
+      });
+  }
+}
